38579b27f2417a7e4992c41ab70bff13b40e3fb3
MTflow <1911849576@qq.com>
2021-04-19 12:40:50 +0800

    init

---
+++ src/main/java/com/mtflow/PropertymanagementbackApplication.java
@@ -0,0 +1,13 @@
+package com.mtflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertymanagementbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertymanagementbackApplication.class, args);
+    }
+
+}
